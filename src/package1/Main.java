827fcc72ae506e557d70fa124dda41459d4a93cd
Kagord <kgordienko73@gmail.com>
2021-12-13 12:38:01 +0300

    Main v1

--- src/package1/Main.java
+++ src/package1/Main.java
@@ -26,15 +26,14 @@ public class Main extends JFrame {
     private static final int HEIGHT = 600;
     // Объект диалогового окна для выбора файлов
     private JFileChooser fileChooser = null;
-    // Пункты меню
     private JCheckBoxMenuItem showAxisMenuItem;
     private JCheckBoxMenuItem showMarkersMenuItem;
-    // Компонент-отображатель графика
+    private JCheckBoxMenuItem rotatePanelMenuItem;
     private GraphicsDisplay display = new GraphicsDisplay();
-    // Флаг, указывающий на загруженность данных графика
     private boolean fileLoaded = false;
+
     public Main() {
-// Вызов конструктора предка Frame
+
         super("Построение графиков функций на основе заранее подготовленных файлов");
 // Установка размеров окна
                 setSize(WIDTH, HEIGHT);
@@ -63,28 +62,40 @@ public class Main extends JFrame {
         }
     };
 // Добавить соответствующий элемент меню
-fileMenu.add(openGraphicsAction);
+        fileMenu.add(openGraphicsAction);
+        //fileMenu.add(openGraphicsAction1);
+
     // Создать пункт меню "График"
     JMenu graphicsMenu = new JMenu("График");
 menuBar.add(graphicsMenu);
     // Создать действие для реакции на активацию элемента "Показывать оси координат"
     Action showAxisAction = new AbstractAction("Показывать оси координат") {
     public void actionPerformed(ActionEvent event) {
-// свойство showAxis класса GraphicsDisplay истина, если элемент меню
-// showAxisMenuItem отмечен флажком, и ложь - в противном случае display.setShowAxis(showAxisMenuItem.isSelected());
+        display.setShowAxis(showAxisMenuItem.isSelected());
     }
 };
-showAxisMenuItem = new JCheckBoxMenuItem(showAxisAction);
+        showAxisMenuItem = new JCheckBoxMenuItem(showAxisAction);
 // Добавить соответствующий элемент в меню
         graphicsMenu.add(showAxisMenuItem);
 // Элемент по умолчанию включен (отмечен флажком)
         showAxisMenuItem.setSelected(true);
 // Повторить действия для элемента "Показывать маркеры точек"
-        Action showMarkersAction = new AbstractAction("Показывать маркеры  точек") {
-public void actionPerformed(ActionEvent event) {
-// по аналогии с showAxisMenuItem display.setShowMarkers(showMarkersMenuItem.isSelected());
-        }
+
+        Action showMarkersAction = new AbstractAction("Показывать маркеры точек") {
+            public void actionPerformed(ActionEvent event) {
+                display.setShowMarkers(showMarkersMenuItem.isSelected());
+            }
         };
+        Action turnGrid = new AbstractAction("Поменять оси") {
+            public void actionPerformed(ActionEvent event) {
+// свойство showAxis класса GraphicsDisplay истина, если элемент меню
+// showAxisMenuItem отмечен флажком, и ложь - в противном случае
+                display.setTurnGrid(rotatePanelMenuItem.isSelected());
+            }
+        };
+        rotatePanelMenuItem = new JCheckBoxMenuItem(turnGrid);
+        graphicsMenu.add(rotatePanelMenuItem);
+        rotatePanelMenuItem.setSelected(false);
         showMarkersMenuItem = new JCheckBoxMenuItem(showMarkersAction);
         graphicsMenu.add(showMarkersMenuItem);
 // Элемент по умолчанию включен (отмечен флажком)
@@ -94,50 +105,33 @@ public void actionPerformed(ActionEvent event) {
 // Установить GraphicsDisplay в цент граничной компоновки
         getContentPane().add(display, BorderLayout.CENTER);
         }
+
 // Считывание данных графика из существующего файла
 protected void openGraphics(File selectedFile) {
         try {
-// Шаг 1 - Открыть поток чтения данных, связанный с входным файловым потоком
-        DataInputStream in = new DataInputStream(new
-        FileInputStream(selectedFile));
-/* Шаг 2 - Зная объѐм данных в потоке ввода можно вычислить,
-* сколько памяти нужно зарезервировать в массиве:
-* Всего байт в потоке - in.available() байт;
-* Размер одного числа Double - Double.SIZE бит, или
-Double.SIZE/8 байт;
-* Так как числа записываются парами, то число пар меньше в
-2 раза
-*/
-        Double[][] graphicsData = new
-        Double[in.available()/(Double.SIZE/8)/2][];
-// Шаг 3 - Цикл чтения данных (пока в потоке есть данные)
-        int i = 0;
-        while (in.available()>0) {
-// Первой из потока читается координата точки X
-        Double x = in.readDouble();
-// Затем - значение графика Y в точке X
-        Double y = in.readDouble();
-// Прочитанная пара координат добавляется в массив
-        graphicsData[i++] = new Double[] {x, y};
-        }
-// Шаг 4 - Проверка, имеется ли в списке в результате чтения хотя бы одна пара координат
-        if (graphicsData!=null && graphicsData.length>0) {
-// Да - установить флаг загруженности данных
-        fileLoaded = true;
-// Вызывать метод отображения графика
-        display.showGraphics(graphicsData);
-        }
-// Шаг 5 - Закрыть входной поток
-        in.close();
+            DataInputStream in = new DataInputStream(new FileInputStream(selectedFile));
+            //Всего байт в потоке - in.available() байт;
+            //Размер числа Double - Double.SIZE бит, или Double.SIZE/8 байт;
+            //Так как числа записываются парами, то число пар меньше в 2 раза
+            Double[][] graphicsData = new Double[in.available()/(Double.SIZE/8)/2][];
+            int i = 0;
+            while (in.available()>0) {
+                Double x = in.readDouble();
+                Double y = in.readDouble();
+                graphicsData[i++] = new Double[] {x, y};
+            }
+            if (graphicsData!=null && graphicsData.length>0) {
+                fileLoaded = true;
+                display.showGraphics(graphicsData);
+            }
+            in.close();
         } catch (FileNotFoundException ex) {
-// В случае исключительной ситуации типа "Файл не найден" показать сообщение об ошибке
-        JOptionPane.showMessageDialog(Main.this, "Указанный  файл не найден", "Ошибка загрузки данных", JOptionPane.WARNING_MESSAGE);
-        return;
+            JOptionPane.showMessageDialog(Main.this, "Указанный файл не найден", "Ошибка загрузки данных", JOptionPane.WARNING_MESSAGE);
+            return;
         } catch (IOException ex) {
-// В случае ошибки ввода из файлового потока показать сообщение об ошибке
-        JOptionPane.showMessageDialog(Main.this, "Ошибка чтения координат точек из файла", "Ошибка загрузки данных",
-        JOptionPane.WARNING_MESSAGE);
-        return;
+            JOptionPane.showMessageDialog(Main.this, "Ошибка чтения координат точек из файла", "Ошибка загрузки данных",
+                    JOptionPane.WARNING_MESSAGE);
+            return;
         }
         }
 public static void main(String[] args) {
